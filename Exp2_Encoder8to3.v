893e404fd7fd993d54a707ac3da56df9c376c437
Christina1106 <161043650+Christina1106@users.noreply.github.com>
2024-04-07 23:01:26 -0700

    Update Exp2_Encoder8to3.v

--- Exp2_Encoder8to3.v
+++ Exp2_Encoder8to3.v
@@ -6,14 +6,14 @@ always @(*)
 begin
 y = 0;
 case(s)
-3'd0: y[0]=a;
-3'd1: y[1]=a;
-3'd2: y[2]=a;
-3'd3: y[3]=a;
-3'd4: y[4]=a;
-3'd5: y[5]=a;
-3'd6: y[6]=a;
-3'd7: y[7]=a;
+3'd0:y[0]=a;
+3'd1:y[1]=a;
+3'd2:y[2]=a;
+3'd3:y[3]=a;
+3'd4:y[4]=a;
+3'd5:y[5]=a;
+3'd6:y[6]=a;
+3'd7:y[7]=a;
 endcase
 end
 endmodule
