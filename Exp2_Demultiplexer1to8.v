7f8ef27ebaa65cfe4ffe656369816a0e6877418a
Christina1106 <161043650+Christina1106@users.noreply.github.com>
2024-04-07 23:06:54 -0700

    Update Exp2_Demultiplexer1to8.v

--- Exp2_Demultiplexer1to8.v
+++ Exp2_Demultiplexer1to8.v
@@ -1,20 +1,22 @@
-module mux_8tol (in, sel, out);
-input [7:0] in: input [2:0] sel;
-output reg out;
+module demux_1_8(y,s,a);
+output reg [7:0]y;
+input [2:0]s;
+input a;
 always @(*)
 begin
-case (sel)
-3'b000: out = in[0];
-3'b001: out = in[1];
-3'b010: out = in[2];
-3'b011: out = in[3];
-3'b100: out = in[4];
-3'b101: out = in[5];
-3'b110: out = in[6];
-3'b111: out = in[7];
-default: out = 1'bx;
+y = 0;
+case(s)
+3'd0:y[0]=a;
+3'd1:y[1]=a;
+3'd2:y[2]=a;
+3'd3:y[3]=a;
+3'd4:y[4]=a;
+3'd5:y[5]=a;
+3'd6:y[6]=a;
+3'd7:y[7]=a;
 endcase
 end
 endmodule
+
 
 
